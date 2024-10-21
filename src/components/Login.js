656e920253d4e9a8ef024e25d070464f70d8f410
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-10-21 16:37:02 +0530

    form validation for email and phone number has done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React, { useState,useRef } from "react";
+import Header from "./Header";
+import {checkValidData} from "../utils/checkValidData"
 
 const Login = () => {
-  const [isSignInForm,setIsSignInForm] =useState(true)
-  const toggleSingInForm =()=>{
-    setIsSignInForm(!isSignInForm)
-  }
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage,setErrorMessage] =useState(null)
+  const email =useRef(null)
+  const password = useRef(null)
 
+  const handleButtonClick=()=>{
+    // validated the data
+    const message =checkValidData(email.current.value,password.current.value)
+     setErrorMessage(message)
+  }
+  const toggleSingInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
 
   return (
     <div className="relative w-full h-screen">
-      <div><Header /></div>
+      <div>
+        <Header />
+      </div>
 
       {/* Background image */}
       <div className="absolute inset-0 z-10">
@@ -23,33 +34,50 @@ const Login = () => {
 
       {/* Login Form */}
       <div className="relative z-20 flex justify-center items-center h-full">
-        <div className="p-14  w-full sm:w-8/12 lg:w-4/12 bg-black bg-opacity-75 text-white  rounded-lg">
-          <h1 className="text-4xl font-bold mb-4">{isSignInForm? "Sign In" :"Sign Up"}</h1>
-          <form className="flex flex-col ">
-            {!isSignInForm && <input
-              type="text"
-              placeholder="Full Name"
-              className="my-4 px-4 py-3 border-[1px] border-red-600 rounded-md bg-gray-800 text-white text-lg "
-            />}
+        <div className="p-14  w-full sm:w-8/12 xl:w-4/12 bg-black bg-opacity-75 text-white  rounded-lg">
+          <h1 className="text-4xl font-bold mb-4">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          <form onSubmit={(e)=>e.preventDefault()} className="flex flex-col ">
+            {!isSignInForm && (
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="my-4 px-4 py-3 border-[1px] border-red-600 rounded-md bg-gray-800 text-white text-lg "
+              />
+            )}
             <input
+            ref={email}
               type="text"
-              placeholder="Email or mobile number"
+              placeholder="Email"
               className="my-4 px-4 py-3 border-[1px] border-red-600 rounded-md bg-gray-800 text-white text-lg "
             />
             <input
+            ref={password}
               type="password"
               placeholder="Password"
               className="my-4 px-4 py-3 border-[1px] border-red-600 rounded-md bg-gray-800 text-white text-lg "
             />
-            <button className="my-2 p-2  text-lg bg-red-600 hover:bg-red-700 transition rounded-md">
-            {isSignInForm? "Sign In" :"Sign Up"}
+            <p className="font-medium text-sm text-red-600">{errorMessage}</p>
+            <button
+              className="my-2 p-2  text-lg bg-red-600 hover:bg-red-700 transition rounded-md"
+              onClick={handleButtonClick}
+            >
+              {isSignInForm ? "Sign In" : "Sign Up"}
             </button>
-            <p className="text-sm cursor-pointer my-2" onClick={toggleSingInForm}>{isSignInForm?"Already Registered ? Sign In Now":"New to Netflix? Sign Up Now"}</p>
+            <p
+              className="text-sm cursor-pointer my-2"
+              onClick={toggleSingInForm}
+            >
+              {isSignInForm
+                ? "Already Registered ? Sign In Now"
+                : "New to Netflix? Sign Up Now"}
+            </p>
           </form>
         </div>
       </div>
     </div>
-  );  
+  );
 };
 
 export default Login;
---
+++ src/utils/checkValidData.js
@@ -0,0 +1,10 @@
+export const checkValidData = (email, password) => {
+    const emailRex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; // Use RegExp object
+    const passwordRex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/; // Use RegExp object
+  
+    if (!emailRex.test(email)) return "❌Please enter a valid email address";
+    if (!passwordRex.test(password)) return "❌Your password must contain between 4 and 60 characters.";
+  
+    return null;
+  };
+  
