7650c020acded401b4ac53a1f97548621f500161
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-10-22 17:49:11 +0530

    video background added

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,12 +3,12 @@ import React from 'react'
 const VideoTitle = (props) => {
   const {title,overview} =props
   return (
-    <div className='pt-36 px-14'>
+    <div className='w-screen aspect-video pt-[15%] px-24  absolute text-white bg-gradient-to-r from-black'>
       <h1 className='text-6xl font-bold'>{title}</h1>
       <p className='py-6 text-lg w-2/5'>{overview}</p>
       <div>
-        <button className='bg-gray-800 bg-opacity-50 text-white p-2  w-36 text-xl rounded-lg mx-6'> ▶ Play</button>
-        <button className='bg-gray-800 bg-opacity-50 text-white p-2  w-36 text-xl rounded-lg mx-6'>More Info</button>
+        <button className='bg-white  text-black  p-2  w-36 text-xl rounded-lg mx-6 hover:bg-opacity-65'> ▶ Play</button>
+        <button className='bg-gray-800  text-white p-2  w-36 text-xl rounded-lg mx-6 hover:bg-opacity-65'>ℹ More Info</button>
       </div>
     </div>
     
